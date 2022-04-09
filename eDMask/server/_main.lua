44c0be61de804b2a80bc14b0a4c98e6025160f3e
eDen <85058066+eDenYZ@users.noreply.github.com>
2022-04-09 15:28:34 +0200

    Update _main.lua

--- eDMask/server/_main.lua
+++ eDMask/server/_main.lua
@@ -16,22 +16,18 @@ AddEventHandler(("%s:buyMask"):format(eDMask.Utils.GetESX), function(pos, price)
         return
     end
     if (_Server.Function.getPos(pos)) then
-        if (#GetEntityCoords(GetPlayerPed(src) - pos) < _Server.Function.Distance) then
-            if (xPlayer.getMoney() >= price) then
-                xPlayer.removeMoney(price)
-                TriggerClientEvent(("%s:buyMask"):format(eDMask.Utils.PrefixEvent), src)
-                TriggerClientEvent(("%s:showNotification"):format(eDMask.Utils.PrefixEvent), src, _Server.Function.Locales["buyMask"])
-            else
-                TriggerClientEvent(("%s:notMoney"):format(eDMask.Utils.PrefixEvent), src)
-                TriggerClientEvent(("%s:showNotification"):format(eDMask.Utils.PrefixEvent), src, _Server.Function.Locales["notEnoughMoney"])
-            end 
+        if (xPlayer.getMoney() >= price) then
+            xPlayer.removeMoney(price)
+            TriggerClientEvent(("%s:buyMask"):format(eDMask.Utils.PrefixEvent), src)
+            TriggerClientEvent(("%s:showNotification"):format(eDMask.Utils.PrefixEvent), src, _Server.Function.Locales["buyMask"])
         else
-            eDMask.Utils.Kick(src, _Server.Function.Locales["cheatDetected"])
-            return
-        end
+            TriggerClientEvent(("%s:notMoney"):format(eDMask.Utils.PrefixEvent), src)
+            TriggerClientEvent(("%s:showNotification"):format(eDMask.Utils.PrefixEvent), src, _Server.Function.Locales["notEnoughMoney"])
+        end 
     else
         eDMask.Utils.Kick(src, _Server.Function.Locales["cheatDetected"])
         return
     end
 end)
+
 
